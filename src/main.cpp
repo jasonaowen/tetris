c3be2cfabb4b0b2c5f31a2ac740c2685e495b08e
Jason Owen <jason.a.owen@gmail.com>
2017-01-26 12:01:56 -0500

    add SDL
    Update the build system and initialize SDL for rendering the game.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
+#include <SDL2/SDL.h>
 
 int main(int argc, char* argv[]) {
+  if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+    SDL_Log("Unable to initialize SDL: %s\n", SDL_GetError());
+    return 1;
+  }
+
   std::cout << "Hello, world!" << std::endl;
+
+  SDL_Quit();
+  return 0;
 }
